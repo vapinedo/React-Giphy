17b4b130a3e8a345e6634d210a21cc91477b02a0
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-11-30 14:35:42 -0500

    GifList comp done

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { GifList } from '@pages/GifList';
 
 export const App = () => {
   return (
@@ -6,6 +7,8 @@ export const App = () => {
       <div className="container-lg main-wrapper">
         <h1>Giphy App</h1>
         <hr />
+
+        <GifList />
       </div>
     </>
   );
---
+++ src/app/pages/GifList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./GifList.css"
+
+export const GifList = () => {
+
+    const gifList = [
+        "Samurai X",
+        "One Punch Man",
+        "Dragon Ball Z",
+        "Hunter x Hunter"
+    ];
+
+    return (
+        <div>
+            <p>GifList works!</p>
+        </div>
+    )
+}
