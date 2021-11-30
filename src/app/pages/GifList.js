be13806f65c22d39fd36464728b02adfd4e13968
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-11-30 16:12:36 -0500

    GifList changes

--- src/app/pages/GifList.js
+++ src/app/pages/GifList.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./GifList.css"
 
 export const GifList = () => {
 
-    const gifList = [
+    const gifCategoryList = [
         "Samurai X",
         "One Punch Man",
         "Dragon Ball Z",
         "Hunter x Hunter"
     ];
 
+    const [gifCategory, setGifCategory] = useState(gifCategoryList);
+
+    const addGifCategory = () => {
+        setGifCategory([...gifCategory, "New category"])
+    };
+
     return (
         <div>
-            <p>GifList works!</p>
+            <h3>GifList works!</h3>
+
+            <ul>
+                {
+                    gifCategory.map(cat => {
+                        return <li key={cat}>{ cat }</li>
+                    })
+                }
+            </ul>
+
+            <button onClick={ addGifCategory }
+                className="btn btn-outline-primary">Add Category</button>
         </div>
     )
 }
