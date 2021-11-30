3e39cf6572061b64f18da4fdb1fce18a39e9c3b3
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-11-30 13:49:51 -0500

    config-override fix typo & app component fix typo too

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,10 @@
 // npm install --save-dev react-app-rewired react-app-rewire-alias
-const { alias } = require("react-app-rewired-alias");
+const { alias } = require("react-app-rewire-alias");
 
 module.exports = function override(config) {
     alias({
         "@root": "src",
+        "@app": "src/app",
         "@hooks": "src/app/hooks",
         "@pages": "src/app/pages",
         "@components": "src/app/components"
