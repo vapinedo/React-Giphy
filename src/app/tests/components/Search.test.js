cc781c6a2512d0d86eb803a0833529f48b695cff
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-06 19:02:11 -0500

    Search compo, unit testing events on input

--- src/app/tests/components/Search.test.js
+++ src/app/tests/components/Search.test.js
@@ -4,10 +4,26 @@ import { Search } from "../../components/Search";
 
 describe("Unit testing for <Search /> component", () => {
 
-    const setCategory = () => {};
+    const setCategory = jest.fn();
+    let wrapper = shallow(<Search setCategory={setCategory} />);
+    
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<Search setCategory={setCategory} />);
+    });
 
     test("Should render correctly", () => {
-        const wrapper = shallow(<Search setCategory={setCategory} />);
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test("Should call onSearch method when changes input value", () => {
+        const input = wrapper.find("input");
+        const value = "Hello world";
+        input.simulate("change", { target: value });
+    });
+
+    test("Should not invoke onSearch method when input is empty", () => {
+        wrapper.find("form").simulate("submit", { preventDefault(){} });
+        expect(setCategory).not.toHaveBeenCalled();
     });
 });
